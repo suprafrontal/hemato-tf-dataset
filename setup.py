27816f52ef79578dc9c19c1602730b2fdc20f1b5
Ali Moeeny <alimoeeny@gmail.com>
2023-01-22 12:28:42 -0500

    remove . from package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='hemato.ai.hemato_tf_dataset',
+    name='hemato_tf_dataset',
     version='1.1.1',
     #scripts=['src/hemato_tf_dataset/'],
     packages=find_packages()
