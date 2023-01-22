df1ddf98590c6287114ceaa51cc17ff6fbffd8a8
Ali Moeeny <alimoeeny@gmail.com>
2023-01-22 12:46:03 -0500

    1.1.3

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
+
+if __name__=="__main__":
+    setup()
 
 setup(
     name='hemato_tf_dataset',
-    version='1.1.2',
+    version='1.1.3',
     #scripts=['src/hemato_tf_dataset/'],
     packages=find_packages()
 )
--- src/hemato_tf_dataset/dataset.py
+++ src/hemato_tf_dataset/dataset.py
@@ -1,4 +1,4 @@
-VERSION="1.1.2"
+VERSION="1.1.3"
 
 class Dataset:
   def version(self):
