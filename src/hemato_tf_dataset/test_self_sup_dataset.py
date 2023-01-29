2a77de843db3ec87bb7400bd2a48611881fce4ab
Ali Moeeny <alimoeeny@gmail.com>
2023-01-29 12:50:48 -0500

    feat:good old fassion array not sequence

--- src/hemato_tf_dataset/test_self_sup_dataset.py
+++ src/hemato_tf_dataset/test_self_sup_dataset.py
@@ -1,3 +1,4 @@
+import numpy
 from hemato_tf_dataset import HemSelfSupDataset
 
 
@@ -5,5 +6,7 @@ def test_HemSelfSupDataset():
     ds = HemSelfSupDataset("tests/test_data", image_width=256)
     assert ds
     assert ds.__len__() == 78
-    x1 = ds[0]
+    x1 = ds.get_batch(0)
     assert len(x1) == ds.batch_size
+    item = ds[0]
+    assert numpy.max(item["img"]) == 1.0
