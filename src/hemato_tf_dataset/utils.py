ced89344043069bb58af3cec3f3febb487586fd6
Ali Moeeny <alimoeeny@gmail.com>
2023-05-24 07:09:57 -0400

    feat:translate_wrap

--- src/hemato_tf_dataset/utils.py
+++ src/hemato_tf_dataset/utils.py
@@ -1,4 +1,5 @@
 import time
+from PIL import Image
 
 
 def deltaT(t1: float, msg: str) -> float:
@@ -6,3 +7,17 @@ def deltaT(t1: float, msg: str) -> float:
     t1 = time.time()
     print(f"\n\x1B[32m ∆ {msg} {delta:0.3f}sec \x1b[0m\n")
     return t1
+
+
+def translate_wrap(image:Image, tx:int, ty:int) -> Image:
+    width, height = image.size
+    new_image = Image.new(image.mode, image.size)
+
+    for x in range(width):
+        for y in range(height):
+            # Apply the translation with wrap-around
+            new_x = (x + tx) % width
+            new_y = (y + ty) % height
+            # Copy the pixel to the new image
+            new_image.putpixel((new_x, new_y), image.getpixel((x, y)))
+    return new_image
