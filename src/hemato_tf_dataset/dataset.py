8a45307d31eb0822389f2d7a79dd8b5f306a9bbc
Ali Moeeny <alimoeeny@gmail.com>
2023-06-06 12:57:57 -0400

    feat: image_distance with channel_weights

--- src/hemato_tf_dataset/dataset.py
+++ src/hemato_tf_dataset/dataset.py
@@ -23,14 +23,19 @@ class Dataset:
         return VERSION
 
 
-def image_distances(a: npt.ArrayLike, b: npt.ArrayLike):
+def image_distances(a: npt.ArrayLike, b: npt.ArrayLike, channel_wights: npt.ArrayLike = [1.0, 1.0, 1.0]):
     """ "
     a and b are arrays of RGB images
     """
+    assert len(a) == len(b)
+    assert len(channel_wights) == 3
+
     a = np.array(a)
     b = np.array(b)
     # absolute_delta = np.sum(np.sum(np.sum([abs(a[i] - b[i]) for i in range(len(a))], axis=3), axis=2), axis=1)
-    absolute_deltas = [np.sum(abs(b[i] - a[i])) for i in range(len(a))]
+    absolute_deltas = [np.sum(((abs(b[i] - a[i])) * channel_wights)) for i in range(len(a))]
     white_image = np.ones(a[0].shape)
-    max_possible_delta = np.sum(white_image)
+    max_possible_delta = np.sum(white_image*np.ceil(channel_wights))
+    if max_possible_delta == 0:
+        return np.zeros(len(a))
     return absolute_deltas / max_possible_delta
--- src/hemato_tf_dataset/test_dataset.py
+++ src/hemato_tf_dataset/test_dataset.py
@@ -1,6 +1,6 @@
 from .dataset import version, image_distances
 import numpy as np
-
+import math
 
 def test_version():
     assert version()
@@ -42,3 +42,65 @@ def test_images_distances_3():
     b = np.zeros((1, 2, 4, 3))
     deltas = image_distances(a, b)
     np.testing.assert_almost_equal(deltas, np.repeat([(2.0 + 2.0 + 2.0 + (0.6 + 0.7 + 0.8) + (0.6 + 0.7 + 0.8)) / (2.0 * 3 * 4)], 1))
+
+def test_images_distances_4():
+    a = np.ones((7, 10, 10, 3))
+    b = np.zeros((7, 10, 10, 3))
+    deltas = image_distances(a, b, channel_wights=[0.0, 0.0, 0.0])
+    assert all([(d == 0) for d in deltas])
+
+    deltas = image_distances(a, b, channel_wights=[0.0, 1.0, 0.0])
+    assert all([(math.isclose(d, 1.)) for d in deltas])
+
+    deltas = image_distances(a, b, channel_wights=[0.0, 1.0, 1.0])
+    assert all([(math.isclose(d, 1.)) for d in deltas])
+
+    deltas = image_distances(a, b, channel_wights=[0.0, 0.27, 0.0])
+    assert all([(math.isclose(d, 0.27)) for d in deltas])
+
+    deltas = image_distances(a, b, channel_wights=[0.0, .6, .09])
+    assert all([(math.isclose(d, 0.345)) for d in deltas])
+
+    deltas = image_distances(a, b, channel_wights=[0.1, .5, .09])
+    assert all([(math.isclose(d, 0.23)) for d in deltas])
+
+def test_images_distances_5():
+    c = 0.6
+    a = np.ones((7, 10, 10, 3))
+    a *= c
+    b = np.zeros((7, 10, 10, 3))
+    deltas = image_distances(a, b, [1.0, 1.0, 1.0])
+    np.testing.assert_almost_equal(deltas, np.repeat([c], 7))
+    deltas = image_distances(a, b, [.0, .0, .0])
+    np.testing.assert_almost_equal(deltas, np.repeat([0], 7))
+    deltas = image_distances(a, b, [1.0, .0, .0])
+    np.testing.assert_almost_equal(deltas, np.repeat([c*1.], 7))
+
+
+def test_images_distances_6():
+    a = [
+        [  # image 1
+            [
+                [1.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0],
+                [1.0, 1.0, 0.0],
+                [0.6, 0.7, 0.8],
+            ],  #
+            [
+                [0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0],
+                [1.0, 1.0, 0.0],
+                [0.6, 0.7, 0.8],
+            ],  #
+        ],
+    ]
+    b = np.zeros((1, 2, 4, 3))
+    deltas = image_distances(a, b)
+    np.testing.assert_almost_equal(deltas, np.repeat([(2.0 + 2.0 + 2.0 + (0.6 + 0.7 + 0.8) + (0.6 + 0.7 + 0.8)) / (2.0 * 3 * 4)], 1))
+    deltas = image_distances(a, b, [1.0, 1.0, 1.0])
+    np.testing.assert_almost_equal(deltas, np.repeat([(2.0 + 2.0 + 2.0 + (0.6 + 0.7 + 0.8) + (0.6 + 0.7 + 0.8)) / (2.0 * 3 * 4)], 1))
+    deltas = image_distances(a, b, [.0, .0, .0])
+    np.testing.assert_almost_equal(deltas, np.repeat([0], 1))
+    deltas = image_distances(a, b, [1.0, .0, .0])
+    np.testing.assert_almost_equal(deltas, np.repeat([0.525], 1))
+
